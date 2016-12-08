6cb7c7e20da43576f0183971191a715f1d24d3bf
Mikael Löthman <mikael.lothman@king.com>
2016-12-08 14:52:58 +0100

    Adds debug script that connects to Docker container.
    This adds debug.sh that connects to a running container and executes bash. Note,
    the container has to be named "extender", which it will be if started with run.sh.

---
+++ scripts/debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONTAINER=$(docker ps -f name=extender -q)
+
+if [ -z "$CONTAINER" ]; then
+    echo Container \"extender\" is not running. Please start it first \(by running run.sh\)!
+else
+    docker exec -it $CONTAINER /bin/bash
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ -z "$DYNAMO_HOME" ]; then
-    docker run -p 9000:9000 extender;
+    docker run --rm --name extender -p 9000:9000 extender;
 else
-    docker run -p 9000:9000 -v ${DYNAMO_HOME}:/dynamo_home -e DYNAMO_HOME=/dynamo_home extender;
+    docker run --rm --name extender -p 9000:9000 -v ${DYNAMO_HOME}:/dynamo_home -e DYNAMO_HOME=/dynamo_home extender;
 fi
-
