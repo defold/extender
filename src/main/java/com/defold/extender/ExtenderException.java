2a0a105660fb2cdbe485608521e4ede4cb9bf0ea
Mathias Westerdahl <mathias.westerdahl@king.com>
2016-12-19 13:34:39 +0100

    Added support for user defined frameworks and defines (#3)
    * DEF-2345 Native Extension: Added source filter for C++ files
    
    * Added support for user defined frameworks and defines
    Added more tests
    
    * Review fixes

--- src/main/java/com/defold/extender/ExtenderException.java
+++ src/main/java/com/defold/extender/ExtenderException.java
@@ -2,6 +2,11 @@ package com.defold.extender;
 
 public class ExtenderException extends Exception {
     private final String output;
+    
+    public ExtenderException(String output) {
+        super(output);
+        this.output = output;
+    }
 
     public ExtenderException(Exception e, String output) {
         super(e);
---
+++ src/main/java/com/defold/extender/ManifestPlatformConfig.java
@@ -0,0 +1,8 @@
+package com.defold.extender;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class ManifestPlatformConfig {
+    public Map<String, Object> context = new HashMap<>();
+}
