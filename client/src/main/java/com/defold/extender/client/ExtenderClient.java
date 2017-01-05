a52245cbec0cf5f12881a7b6ca8c4f7dc8ef7dd0
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-01-05 16:08:43 +0100

    Review fix

--- client/src/main/java/com/defold/extender/client/ExtenderClient.java
+++ client/src/main/java/com/defold/extender/client/ExtenderClient.java
@@ -1,6 +1,5 @@
 package com.defold.extender.client;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
