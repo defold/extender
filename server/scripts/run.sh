86d95c1119981d0fe31c7ea334baabf26d254aba
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-01-16 10:52:24 +0100

    Changed container name (#12)

--- server/scripts/run.sh
+++ server/scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$DYNAMO_HOME" ]; then
-    docker run --rm --name extender -p 9000:9000 extender;
+    docker run --rm --name extender -p 9000:9000 extender/extender;
 else
-    docker run --rm --name extender -p 9000:9000 -v ${DYNAMO_HOME}:/dynamo_home -e DYNAMO_HOME=/dynamo_home extender;
+    docker run --rm --name extender -p 9000:9000 -v ${DYNAMO_HOME}:/dynamo_home -e DYNAMO_HOME=/dynamo_home extender/extender;
 fi
