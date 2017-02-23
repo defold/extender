8e32f6aa484257a88a93f4bc6dfecb108c4547ed
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-02-23 14:27:23 +0100

    Fixed issue of not allowing '+' in filenames (#25)

--- server/src/main/java/com/defold/extender/ExtenderController.java
+++ server/src/main/java/com/defold/extender/ExtenderController.java
@@ -29,7 +29,7 @@ public class ExtenderController {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExtenderController.class);
 
     // Used to verify the uploaded filenames
-    private static final Pattern FILENAME_RE = Pattern.compile("^([A-Za-z0-9_ ](?:[A-Za-z0-9_\\-/ ]|(?:\\.[A-Za-z0-9_\\-/ ]))+)$");
+    private static final Pattern FILENAME_RE = Pattern.compile("^([A-Za-z0-9_ ](?:[A-Za-z0-9_+\\-/ ]|(?:\\.[A-Za-z0-9_+\\-/ ]))+)$");
 
     private final DefoldSdkService defoldSdkService;
 
