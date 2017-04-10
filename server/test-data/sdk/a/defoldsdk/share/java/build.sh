d58b6738da6dd872ec65988a8cda8c2aa4dd6a37
Mathias Westerdahl <mathias.westerdahl@king.com>
2017-04-10 16:06:09 +0200

    DEF-2639 review fixes

--- server/test-data/sdk/a/defoldsdk/share/java/build.sh
+++ server/test-data/sdk/a/defoldsdk/share/java/build.sh
@@ -1,9 +1,3 @@
-# DEBUG
-# Dump embedded resources:
-# /Users/mathiaswesterdahl/android//android-sdk/build-tools/23.0.2/aapt d resources AndroidNativeExt/AndroidNativeExt.apk 
-
-BUILD=./build
-AAPT=~/android/android-sdk/build-tools/23.0.2/aapt
 
 javac  -source 1.6 -target 1.6  -cp . com/defoldtest/engine/Engine.java
 jar cvf Engine.jar com
