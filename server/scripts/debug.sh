64f642209c168ce07938d20d068879add64624f1
Mathias Westerdahl <mathias.westerdahl@king.com>
2018-03-12 10:57:53 +0100

    Added readme for debugging command lines

--- server/scripts/debug.sh
+++ server/scripts/debug.sh
@@ -5,5 +5,5 @@ CONTAINER=$(docker ps -f name=extender -q)
 if [ -z "$CONTAINER" ]; then
     echo Container \"extender\" is not running. Please start it first \(by running run.sh\)!
 else
-    docker exec -it $CONTAINER /bin/bash
+    docker exec -uextender -it $CONTAINER /bin/bash
 fi
