3a0ac2c82a66f97a2bcb6f78f4ac0afe439b1819
Jonas Thurfors <jonas.thurfors@king.com>
2018-06-21 10:37:26 +0200

    Configure Jetty http connector idle timeout programatically

--- server/src/main/java/com/defold/extender/ExtenderApplication.java
+++ server/src/main/java/com/defold/extender/ExtenderApplication.java
@@ -1,20 +1,27 @@
 package com.defold.extender;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.ServerConnector;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
 import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
 import org.springframework.boot.actuate.endpoint.MetricsEndpointMetricReader;
 import org.springframework.boot.actuate.metrics.writer.GaugeWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -24,13 +31,15 @@ public class ExtenderApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExtenderApplication.class);
 
     private final Environment environment;
+    private final int idleTimeout;
 
     @Autowired
-    public ExtenderApplication(Environment environment) {
+    public ExtenderApplication(Environment environment, @Value("${extender.server.http.idle-timeout}") int idleTimeout) {
         this.environment = environment;
+        this.idleTimeout = idleTimeout;
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(ExtenderApplication.class, args);
     }
 
@@ -75,5 +84,33 @@ public class ExtenderApplication {
     @Bean
     public MetricsEndpointMetricReader metricsEndpointMetricReader(MetricsEndpoint metricsEndpoint) {
         return new MetricsEndpointMetricReader(metricsEndpoint);
+    }
+
+    // Spring Boot only supports a subset of Jetty configuration props, so configure idle timeout programatically
+    @Bean
+    public EmbeddedServletContainerCustomizer customizer() {
+        return new EmbeddedServletContainerCustomizer() {
+
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                if (container instanceof JettyEmbeddedServletContainerFactory) {
+                    customizeJetty((JettyEmbeddedServletContainerFactory) container);
+                }
+            }
+
+            private void customizeJetty(JettyEmbeddedServletContainerFactory jetty) {
+                jetty.addServerCustomizers((JettyServerCustomizer) server -> {
+                    for (Connector connector : server.getConnectors()) {
+                        if (connector instanceof ServerConnector) {
+                            HttpConnectionFactory connectionFactory = connector
+                                    .getConnectionFactory(HttpConnectionFactory.class);
+                            connectionFactory.getHttpConfiguration()
+                                    .setIdleTimeout(idleTimeout);
+                        }
+                    }
+                });
+            }
+        };
+
     }
 }
