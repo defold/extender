cd8f9055b2aa606db97d8bde46509417e7651557
Mathias Westerdahl <mathias.westerdahl@king.com>
2018-08-20 16:39:51 +0200

    DEF-3126 Added support for windres command for Win32 builds

--- server/scripts/build.sh
+++ server/scripts/build.sh
@@ -6,4 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t extender-base ${DIR}/../docker-base
 
-${DIR}/../../gradlew clean buildDocker --info
+${DIR}/../../gradlew clean buildDocker --info $@
