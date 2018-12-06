cbcb50d295f3d6a1f8b1c53f0954ad11adef0878
mathiaswking <mathias.westerdahl@king.com>
2018-12-06 14:26:16 +0100

    More test fixes

--- server/src/test/java/com/defold/extender/cache/info/CacheInfoFileParserTest.java
+++ server/src/test/java/com/defold/extender/cache/info/CacheInfoFileParserTest.java
@@ -1,6 +1,7 @@
 package com.defold.extender.cache.info;
 
 import com.defold.extender.cache.CacheEntry;
+import com.defold.extender.TestUtils;
 import org.junit.Test;
 
 import java.io.File;
@@ -20,12 +21,12 @@ public class CacheInfoFileParserTest {
         assertEquals(2, entries.size());
 
         CacheEntry entry1 = entries.get(0);
-        assertEquals("dir/test1.txt", entry1.getPath());
-        assertEquals("LYwvbZeMohcStfbeNsnTH6jpak-l2P-LAYjfuefBcbs", entry1.getKey());
+        assertEquals(TestUtils.CACHE_ENTRIES[0].getPath(), entry1.getPath());
+        assertEquals(TestUtils.CACHE_ENTRIES[0].getKey(), entry1.getKey());
 
         CacheEntry entry2 = entries.get(1);
-        assertEquals("dir2/test2.txt", entry2.getPath());
-        assertEquals("fzthrrNKjqFcZ1_92qavam-90DHtl4bcsrNbNRoTKzE", entry2.getKey());
+        assertEquals(TestUtils.CACHE_ENTRIES[1].getPath(), entry2.getPath());
+        assertEquals(TestUtils.CACHE_ENTRIES[1].getKey(), entry2.getKey());
     }
 
     @Test
@@ -37,11 +38,11 @@ public class CacheInfoFileParserTest {
         assertEquals(2, entries.size());
 
         CacheEntry entry1 = entries.get(0);
-        assertEquals("dir/test1.txt", entry1.getPath());
-        assertEquals("LYwvbZeMohcStfbeNsnTH6jpak-l2P-LAYjfuefBcbs", entry1.getKey());
+        assertEquals(TestUtils.CACHE_ENTRIES[0].getPath(), entry1.getPath());
+        assertEquals(TestUtils.CACHE_ENTRIES[0].getKey(), entry1.getKey());
 
         CacheEntry entry2 = entries.get(1);
-        assertEquals("dir2/test2.txt", entry2.getPath());
-        assertEquals("fzthrrNKjqFcZ1_92qavam-90DHtl4bcsrNbNRoTKzE", entry2.getKey());
+        assertEquals(TestUtils.CACHE_ENTRIES[1].getPath(), entry2.getPath());
+        assertEquals(TestUtils.CACHE_ENTRIES[1].getKey(), entry2.getKey());
     }
 }
