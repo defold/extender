679123501b1c428c8c891aecf0fffa9a18839a99
mathiaswking <mathias.westerdahl@king.com>
2019-02-07 11:43:41 +0100

    fix

--- server/test-data/ext_use_base_extension/src/test_ext.cpp
+++ server/test-data/ext_use_base_extension/src/test_ext.cpp
@@ -1,9 +1,12 @@
 
-#include <ext/include/ext.h>
+// The "test-data" folder is part of the upload folder in our integration tests
+// For actual extensions it would be "ext/include/ext.h"
+#include <test-data/ext/include/ext.h>
+#include <stdio.h>
 
 extern "C"
 {
-	void TestUseBaseExtension()
+	void ext_use_base_extension() // Need to have the symbol specified in the ext.manifest
 	{
         printf("Hello From TestUseBaseExtension\n");
         Test();
