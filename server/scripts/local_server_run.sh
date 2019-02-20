d078ca88eecc0a911ce274ee11a3dff4a4e3d732
mathiaswking <mathias.westerdahl@king.com>
2019-02-20 14:03:35 +0100

    Updated local dev scripts

--- server/scripts/local_server_run.sh
+++ server/scripts/local_server_run.sh
@@ -8,4 +8,4 @@ if [ "$PLATFORMSDK_DIR" == "" ]; then
 	echo "Found platformsdk:" $PLATFORMSDK_DIR
 fi
 
-PLATFORMSDK_DIR=${PLATFORMSDK_DIR} PATH=${PLATFORMSDK_DIR}/XcodeDefault10.1.xctoolchain/usr/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin java -jar -Dspring.profiles.active=standalone-dev $PWD/server/build/docker/extender-0.1.0.jar
+PLATFORMSDK_DIR=${PLATFORMSDK_DIR} PATH=${PLATFORMSDK_DIR}/XcodeDefault10.1.xctoolchain/usr/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin java -jar -Dspring.profiles.active=standalone,standalone-dev $PWD/server/build/docker/extender-0.1.0.jar
