25dee15ab3abddab12b0668a09daf8d122613d7f
Jonas Thurfors <jonas.thurfors@king.com>
2019-03-08 11:00:33 +0100

    Be able to start the extender service if a pid file exists but no process is actually running

--- server/scripts/standalone/service-standalone.sh
+++ server/scripts/standalone/service-standalone.sh
@@ -16,11 +16,11 @@ export PLATFORMSDK_DIR=${EXTENDER_DIR}/platformsdk
 export PATH=${PLATFORMSDK_DIR}/XcodeDefault10.1.xctoolchain/usr/bin:/usr/local/bin:${PATH}
 
 # Get the Spring profile from the environment file
-if [ -f "${ENVIRONMENT_FILE}" ]
+if [[ -f "${ENVIRONMENT_FILE}" ]]
 then
     while IFS='=' read -r key value
     do
-        if [ "${key}" = "profile" ]; then
+        if [[ "${key}" = "profile" ]]; then
             PROFILE=$(echo ${value})
             echo "Using profile $PROFILE"
         fi
@@ -30,32 +30,40 @@ else
     exit 1;
 fi
 
-if [ -z ${PROFILE} ]
-then
-    echo "Error! Environment file ${ENVIRONMENT_FILE} not found, exiting."
+if [[ -z ${PROFILE} ]]; then
+    echo "Error! No Spring profile set in environment file ${ENVIRONMENT_FILE}, exiting."
     exit 1;
 fi
 
 start_service() {
     echo "${SERVICE_NAME} starting..."
-    if [ ! -f ${PID_PATH_NAME} ]; then
-        nohup java -Xmx1g -XX:MaxDirectMemorySize=512m -jar ${PATH_TO_JAR} --spring.profiles.active=${PROFILE} >> ${STDOUT_LOG} 2>> ${ERROR_LOG} < /dev/null &
-        echo $! > ${PID_PATH_NAME}
-        echo "${SERVICE_NAME} started."
-    else
-        echo "${SERVICE_NAME} is already running."
+
+    # Check if pid file already exists and if the process in the pid file is running
+    if [[ -f ${PID_PATH_NAME} ]]; then
+        PID=$(cat ${PID_PATH_NAME});
+        if [[ -n "$(ps -p ${PID} -o pid=)" ]]; then
+            echo "Error! ${SERVICE_NAME} is already running."
+            exit 2
+        else
+            echo "Warning: PID file exists but no process running, removing PID file."
+            rm ${PID_PATH_NAME}
+        fi
     fi
+
+    nohup java -Xmx1g -XX:MaxDirectMemorySize=512m -jar ${PATH_TO_JAR} --spring.profiles.active=${PROFILE} >> ${STDOUT_LOG} 2>> ${ERROR_LOG} < /dev/null &
+    echo $! > ${PID_PATH_NAME}
+    echo "${SERVICE_NAME} started."
 }
 
 stop_service() {
-    if [ -f ${PID_PATH_NAME} ]; then
+    if [[ -f ${PID_PATH_NAME} ]]; then
         PID=$(cat ${PID_PATH_NAME});
         echo "${SERVICE_NAME} stopping..."
         kill ${PID};
         echo "${SERVICE_NAME} stopped."
         rm ${PID_PATH_NAME}
     else
-        echo "${SERVICE_NAME} is not running."
+        echo "Warning: ${SERVICE_NAME} is not running, no PID file."
     fi
 }
 
