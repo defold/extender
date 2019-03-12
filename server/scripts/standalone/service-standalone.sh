b1cf50edb210bfcc8fe5807820900dae8b111d37
Jonas Thurfors <jonas.thurfors@king.com>
2019-03-12 14:29:08 +0100

    Increase max heap memory 512m -> 1g, set direct byte buffer to 512m

--- server/scripts/standalone/service-standalone.sh
+++ server/scripts/standalone/service-standalone.sh
@@ -50,7 +50,7 @@ start_service() {
         fi
     fi
 
-    nohup java -jar ${PATH_TO_JAR} --spring.profiles.active=${PROFILE} >> ${STDOUT_LOG} 2>> ${ERROR_LOG} < /dev/null &
+    nohup java -Xmx1g -XX:MaxDirectMemorySize=512m -jar ${PATH_TO_JAR} --spring.profiles.active=${PROFILE} >> ${STDOUT_LOG} 2>> ${ERROR_LOG} < /dev/null &
     echo $! > ${PID_PATH_NAME}
     echo "${SERVICE_NAME} started."
 }
