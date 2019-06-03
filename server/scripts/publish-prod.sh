54646c643ebdb8366b61c77d4bdab35045ded297
Mathias Westerdahl <mathias.westerdahl@king.com>
2019-06-03 14:11:38 +0200

    Added version to the extender info page (#137)
    * Added version and build time to extender info page
    
    * Publishing script now output the version of the newly running server
    
    * review fix

--- server/scripts/publish-prod.sh
+++ server/scripts/publish-prod.sh
@@ -5,3 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${DIR}/shared/publi.sh
 
 publish extender prod
+
+SERVER=https://build.defold.com
+
+echo "**********************************"
+echo "Checking the server version:"
+wget -q -O - $SERVER
+echo ""
+echo "**********************************"
