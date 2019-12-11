f6da63a91473e07abe141fcfcfaa63332ac9b66d
JCash <mwesterdahl76@gmail.com>
2019-12-11 12:12:36 +0100

    Added aaptExtraPackages as a valid keyword to extension contexts

--- server/src/main/java/com/defold/extender/PlatformConfig.java
+++ server/src/main/java/com/defold/extender/PlatformConfig.java
@@ -23,6 +23,7 @@ class PlatformConfig {
     public String javacCmd;
     public String jarCmd;
     public String dxCmd;
+    public String rjavaCmd;
     public String mtCmd;    // Deprecated, use windres instead (Deprecated at 1.2.135)
     public String proGuardSourceRe;
     public String proGuardCmd = new String();
