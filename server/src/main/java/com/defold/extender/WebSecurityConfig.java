ae43eb4a488d4b6d1566d4d7c6f8dba98b9215ae
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-03-12 09:39:27 +0100

    Configurable platforms for basic auth

--- server/src/main/java/com/defold/extender/WebSecurityConfig.java
+++ server/src/main/java/com/defold/extender/WebSecurityConfig.java
@@ -18,7 +18,10 @@ import org.springframework.core.io.Resource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Value("file:users.txt")
+	@Value("${extender.authentication.platforms}")
+	String[] authenticatedPlatforms;
+
+	@Value("${extender.authentication.users}")
 	Resource usersResource;
 
 	private Properties loadUsers() throws Exception {
@@ -31,26 +34,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		Properties users = loadUsers();
-		if (!users.isEmpty()) {
-			http.authorizeRequests().antMatchers("/build/armv7-android/**").hasRole("ANDROID").and().httpBasic();
-			http.authorizeRequests().antMatchers("/build/arm64-android/**").hasRole("ANDROID").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/armv7-ios:/**").hasRole("IOS").and().httpBasic();
-			http.authorizeRequests().antMatchers("/build/arm64-ios/**").hasRole("IOS").and().httpBasic();
-			http.authorizeRequests().antMatchers("/build/x86_64-ios/**").hasRole("IOS").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/x86_64-linux/**").hasRole("LINUX").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/x86_64-osx/**").hasRole("MACOS").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/x86_64-win32/**").hasRole("WINDOWS").and().httpBasic();
-			http.authorizeRequests().antMatchers("/build/x86-win32/**").hasRole("WINDOWS").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/js-web/**").hasRole("HTML5").and().httpBasic();
-			http.authorizeRequests().antMatchers("/build/wasm-web/**").hasRole("HTML5").and().httpBasic();
-
-			http.authorizeRequests().antMatchers("/build/arm64-nx64/**").hasRole("SWITCH").and().httpBasic();
+		for(String platform : authenticatedPlatforms) {
+			switch(platform) {
+				case "android":
+					http.authorizeRequests().antMatchers("/build/armv7-android/**").hasRole("ANDROID").and().httpBasic();
+					http.authorizeRequests().antMatchers("/build/arm64-android/**").hasRole("ANDROID").and().httpBasic();
+					break;
+				case "ios":
+					http.authorizeRequests().antMatchers("/build/armv7-ios:/**").hasRole("IOS").and().httpBasic();
+					http.authorizeRequests().antMatchers("/build/arm64-ios/**").hasRole("IOS").and().httpBasic();
+					http.authorizeRequests().antMatchers("/build/x86_64-ios/**").hasRole("IOS").and().httpBasic();
+					break;
+				case "linux":
+					http.authorizeRequests().antMatchers("/build/x86_64-linux/**").hasRole("LINUX").and().httpBasic();
+					break;
+				case "macos":
+					http.authorizeRequests().antMatchers("/build/x86_64-osx/**").hasRole("MACOS").and().httpBasic();
+					break;
+				case "windows":
+					http.authorizeRequests().antMatchers("/build/x86_64-win32/**").hasRole("WINDOWS").and().httpBasic();
+					http.authorizeRequests().antMatchers("/build/x86-win32/**").hasRole("WINDOWS").and().httpBasic();
+					break;
+				case "html5":
+					http.authorizeRequests().antMatchers("/build/js-web/**").hasRole("HTML5").and().httpBasic();
+					http.authorizeRequests().antMatchers("/build/wasm-web/**").hasRole("HTML5").and().httpBasic();
+					break;
+				case "switch":
+					http.authorizeRequests().antMatchers("/build/arm64-nx64/**").hasRole("SWITCH").and().httpBasic();
+					break;
+			}
 		}
 		http.csrf().disable();
 	}
