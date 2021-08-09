7689e2fffd2b5c3522fed50d840e132e4e8334d2
JCash <mwesterdahl76@gmail.com>
2021-08-09 16:07:51 +0200

    Add executable bit when extracting the Defold sdk

--- server/src/main/java/com/defold/extender/ZipUtils.java
+++ server/src/main/java/com/defold/extender/ZipUtils.java
@@ -3,19 +3,47 @@ package com.defold.extender;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.HashSet;
+
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
+
+// For reading and preserving attributes
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 
 public class ZipUtils {
 
     private static int bufferSize = 128 * 1024;
 
+    public static Set<PosixFilePermission> getPosixFilePermissions(int mode) {
+        final PosixFilePermission[] values = {
+            PosixFilePermission.OTHERS_EXECUTE, PosixFilePermission.OTHERS_WRITE, PosixFilePermission.OTHERS_READ,
+            PosixFilePermission.GROUP_EXECUTE, PosixFilePermission.GROUP_WRITE, PosixFilePermission.GROUP_READ,
+            PosixFilePermission.OWNER_EXECUTE, PosixFilePermission.OWNER_WRITE, PosixFilePermission.OWNER_READ
+        };
+
+        int mask = 1;
+
+        Set<PosixFilePermission> s = EnumSet.noneOf(PosixFilePermission.class);
+        for (PosixFilePermission p : values) {
+            if ((mask & mode) != 0) {
+                s.add(p);
+            }
+            mask = mask << 1;
+        }
+        return s;
+    }
+
     public static void unzip(InputStream inputStream, Path targetDirectory) throws IOException {
-        try (ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
+        try (ZipArchiveInputStream zipInputStream = new ZipArchiveInputStream(inputStream)) {
+            ZipArchiveEntry zipEntry = zipInputStream.getNextZipEntry();
 
             while (zipEntry != null) {
                 File entryTargetFile = new File(targetDirectory.toFile(), zipEntry.getName());
@@ -28,10 +56,25 @@ public class ZipUtils {
                         Files.createDirectories(parentDir.toPath());
                     }
                     extractFile(zipInputStream, entryTargetFile);
+
+                    // TODO: Find out why it doesn't return other than 0 !?
+                    //int permissions = zipEntry.getUnixMode();
+                    //Set<PosixFilePermission> s = getPosixFilePermissions(permissions);
+                    Set<PosixFilePermission> s = new HashSet<>();
+                    s.add(PosixFilePermission.OTHERS_READ);
+                    s.add(PosixFilePermission.GROUP_READ);
+                    s.add(PosixFilePermission.OWNER_READ);
+
+                    // Poor man's version of making sure the stuff in the bin folder is executable
+                    if (entryTargetFile.getAbsolutePath().contains("/bin/")) {
+                        s.add(PosixFilePermission.GROUP_EXECUTE);
+                        s.add(PosixFilePermission.OWNER_EXECUTE);
+                    }
+
+                    Files.setPosixFilePermissions(entryTargetFile.toPath(), s);
                 }
 
-                zipInputStream.closeEntry();
-                zipEntry = zipInputStream.getNextEntry();
+                zipEntry = zipInputStream.getNextZipEntry();
             }
         }
     }
@@ -84,6 +127,16 @@ public class ZipUtils {
     }
 
     private static void extractFile(ZipInputStream zipIn, File file) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+        byte[] bytesIn = new byte[ZipUtils.bufferSize];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+
+    private static void extractFile(ZipArchiveInputStream zipIn, File file) throws IOException {
         BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
         byte[] bytesIn = new byte[ZipUtils.bufferSize];
         int read = 0;
