2205f6910864df3bc78a56529e5324b94e6cc5e3
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-09-23 13:24:52 +0200

    Differentiate between url and ec2 instance

--- server/scripts/publish-darwin-prod.sh
+++ server/scripts/publish-darwin-prod.sh
@@ -5,7 +5,8 @@ SOURCE_DIR=${SCRIPT_DIR}/../..
 
 VERSION=$(date "+%Y%m%d_%H%M")
 
-TARGET_HOST=build-darwin.defold.com
+TARGET_HOST_URL=build-darwin.defold.com
+TARGET_HOST=ec2-52-48-199-151.eu-west-1.compute.amazonaws.com
 TARGET_USER=ec2-user
 TARGET_DIR=/usr/local/extender
 TARGET_KEY=~/.ssh/defold2_ec2.pem
@@ -17,7 +18,7 @@ check_uncommitted_changes ${SOURCE_DIR}
 build_artifact ${SOURCE_DIR}
 deploy_artifact ${SOURCE_DIR} ${TARGET_DIR} ${VERSION} ${TARGET_HOST} ${TARGET_USER} ${TARGET_KEY}
 
-SERVER=https://${TARGET_HOST}
+SERVER=https://${TARGET_HOST_URL}
 
 echo "**********************************"
 echo "Checking the server version:"
