42ddfaef51c89b857272dc771f9ebe5ed739fa30
Alexey Gulev <me@agulev.com>
2022-04-06 10:34:21 +0200

    remove old tests and replace old SDKs

--- server/src/test/java/com/defold/extender/AuthenticationTest.java
+++ server/src/test/java/com/defold/extender/AuthenticationTest.java
@@ -31,7 +31,7 @@ import static org.junit.Assert.*;
 public class AuthenticationTest {
 
     private static final int EXTENDER_PORT = 9000;
-    private static final String SDK_VERSION = "d31d6397a72178541a5ef6e7ef2bed090d828f58"; // 1.2.190
+    private static final String SDK_VERSION = "d393bae6a361f86cf2263ab312c9b3cea45253ab"; // 1.2.191
     private static final String PLATFORM_ARMV7_ANDROID = "armv7-android";
     private static final String PLATFORM_LINUX = "x86_64-linux";
     private static final String PLATFORM_WIN32 = "x86_64-win32";
