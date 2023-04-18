ab32cf9f5baaa8c5c9d1e44361464a5bb19710c1
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-04-18 15:54:36 +0200

    Fix standalone scripts (#281)
    * Allow override of extender.sdk.location
    
    * Create target dir bin folder
    
    * Make target dir configurable

--- server/scripts/rebuild-and-run-standalone-local.sh
+++ server/scripts/rebuild-and-run-standalone-local.sh
@@ -1,13 +1,27 @@
 #!/bin/bash -e
 
-if [ -f "/usr/local/extender/current/service.sh" ]; then
+TARGET_DIR=$1
+if [[ -z ${TARGET_DIR} ]]; then
+	echo "Missing target dir"
+	exit 1
+fi
+
+# override extender.sdk.location from application-standalone-dev.yml by
+# setting EXTENDER_SDK_LOCATION and using it in service-standalone.sh
+export TARGET_DIR=${TARGET_DIR}
+export EXTENDER_SDK_LOCATION=${TARGET_DIR}/sdk
+
+echo "Rebuilding and running standalone local extender from ${TARGET_DIR}"
+
+mkdir -p ${TARGET_DIR}
+if [ -f "${TARGET_DIR}/current/service.sh" ]; then
 	echo "Stopping server"
-	/usr/local/extender/current/service.sh stop /usr/local/extender
+	${TARGET_DIR}/current/service.sh stop ${TARGET_DIR}
 fi
 
 echo "Building server"
 ./server/scripts/build-standalone.sh -xtest
 
 echo "Running server"
-./server/scripts/run-standalone-local.sh
-tail -f /usr/local/extender/logs/stdout.log
+./server/scripts/run-standalone-local.sh ${TARGET_DIR}
+tail -f ${TARGET_DIR}/logs/stdout.log
