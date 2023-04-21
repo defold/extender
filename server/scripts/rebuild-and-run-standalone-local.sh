5615a3741a89b2614ce0aadde89de9fe725ac8b5
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-04-21 08:21:35 +0200

    Set env variables

--- server/scripts/rebuild-and-run-standalone-local.sh
+++ server/scripts/rebuild-and-run-standalone-local.sh
@@ -11,6 +11,31 @@ fi
 export TARGET_DIR=${TARGET_DIR}
 export EXTENDER_SDK_LOCATION=${TARGET_DIR}/sdk
 
+export ANDROID_HOME=~/android/sdk
+export ANDROID_SDK_ROOT=${ANDROID_HOME}
+export ANDROID_BUILD_TOOLS_VERSION=33.0.1
+export ANDROID_SDK_VERSION=33
+export ANDROID_SDK_BUILD_TOOLS_PATH=${ANDROID_HOME}/build-tools/${ANDROID_BUILD_TOOLS_VERSION}
+export ANDROID_LIBRARYJAR=${ANDROID_HOME}/platforms/android-${ANDROID_SDK_VERSION}/android.jar
+export ANDROID_PROGUARD=_
+export MANIFEST_MERGE_TOOL=${TARGET_DIR}/current/manifestmergetool.jar
+
+export ANDROID_NDK25_VERSION=25b
+export ANDROID_NDK25_FULLVERSION=25.1.8937393
+export ANDROID_NDK25_API_VERSION=19
+export ANDROID_64_NDK25_API_VERSION=21
+export ANDROID_NDK25_PATH=${ANDROID_HOME}/ndk/${ANDROID_NDK25_FULLVERSION}
+export ANDROID_NDK25_BIN_PATH=${ANDROID_NDK25_PATH}/toolchains/llvm/prebuilt/darwin-x86_64/bin
+export ANDROID_NDK25_SYSROOT=${ANDROID_NDK25_PATH}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot
+
+export GRADLE_USER_HOME=/tmp/.gradle
+export GRADLE_PLUGIN_VERSION=4.0.1
+
+export EXTENSION_BUILD_GRADLE_TEMPLATE=${TARGET_DIR}/current/template.build.gradle
+export EXTENSION_GRADLE_PROPERTIES_TEMPLATE=${TARGET_DIR}/current/template.gradle.properties
+export EXTENSION_LOCAL_PROPERTIES_TEMPLATE=${TARGET_DIR}/current/template.local.properties
+
+
 echo "Rebuilding and running standalone local extender from ${TARGET_DIR}"
 
 mkdir -p ${TARGET_DIR}
