ebe2c475983251883092c13f5d7a1a89c3704199
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-06-30 08:52:34 +0200

    Get inherited parent spec values

--- server/src/main/java/com/defold/extender/services/CocoaPodsService.java
+++ server/src/main/java/com/defold/extender/services/CocoaPodsService.java
@@ -48,45 +48,71 @@ public class CocoaPodsService {
         public File frameworksDir;
         public String platformMinVersion;
 
+        // In the functions beloe we also get the values from the parent spec
+        // if one exists. A parent spec inherits all of its subspecs (unless a 
+        // default_spec is set). And the subspecs inherit the values of their
+        // parent
+        // https://guides.cocoapods.org/syntax/podspec.html#subspec
+
+        private void addPodLibs(String platform, PodSpec pod, Set<String> libs) {
+            libs.addAll(pod.libraries.get(platform));
+            if (pod.parentSpec != null) addPodLibs(platform, pod.parentSpec, libs);
+        }
         public List<String> getAllPodLibs(String platform) {
             Set<String> libs = new LinkedHashSet<>();
             for (PodSpec pod : pods) {
-                libs.addAll(pod.libraries.get(platform));
+                addPodLibs(platform, pod, libs);
             }
             return new ArrayList<String>(libs);
         }
 
+        private void addPodLinkFlags(String platform, PodSpec pod, Set<String> flags) {
+            flags.addAll(pod.linkflags.get(platform));
+            if (pod.parentSpec != null) addPodLinkFlags(platform, pod.parentSpec, flags);
+        }
         public List<String> getAllPodLinkFlags(String platform) {
             Set<String> flags = new LinkedHashSet<>();
             for (PodSpec pod : pods) {
-                flags.addAll(pod.linkflags.get(platform));
+                addPodLinkFlags(platform, pod, flags);
             }
             return new ArrayList<String>(flags);
         }
 
+        private void addPodResources(String platform, PodSpec pod, Set<String> resources) {
+            String podDir = pod.dir.getAbsolutePath();
+            for (String resource : pod.resources.get(platform)) {
+                resources.add(podDir + "/" + resource);
+            }
+            if (pod.parentSpec != null) addPodResources(platform, pod.parentSpec, resources);
+        }
         public List<String> getAllPodResources(String platform) {
             Set<String> resources = new LinkedHashSet<>();
             for (PodSpec pod : pods) {
-                String podDir = pod.dir.getAbsolutePath();
-                for (String resource : pod.resources.get(platform)) {
-                    resources.add(podDir + "/" + resource);
-                }
+                addPodResources(platform, pod, resources);
             }
             return new ArrayList<String>(resources);
         }
 
+        private void addPodFrameworks(String platform, PodSpec pod, Set<String> frameworks) {
+            frameworks.addAll(pod.frameworks.get(platform));
+            if (pod.parentSpec != null) addPodFrameworks(platform, pod.parentSpec, frameworks);
+        }
         public List<String> getAllPodFrameworks(String platform) {
             Set<String> frameworks = new LinkedHashSet<>();
             for (PodSpec pod : pods) {
-                frameworks.addAll(pod.frameworks.get(platform));
+                addPodFrameworks(platform, pod, frameworks);
             }
             return new ArrayList<String>(frameworks);
         }
 
+        private void addPodWeakFrameworks(String platform, PodSpec pod, Set<String> weakFrameworks) {
+            weakFrameworks.addAll(pod.weak_frameworks.get(platform));
+            if (pod.parentSpec != null) addPodWeakFrameworks(platform, pod.parentSpec, weakFrameworks);
+        }
         public List<String> getAllPodWeakFrameworks(String platform) {
             Set<String> weakFrameworks = new LinkedHashSet<>();
             for (PodSpec pod : pods) {
-                weakFrameworks.addAll(pod.weak_frameworks.get(platform));
+                addPodWeakFrameworks(platform, pod, weakFrameworks);
             }
             return new ArrayList<String>(weakFrameworks);
         }
