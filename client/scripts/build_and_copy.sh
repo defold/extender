29d1296f009cc235b58e2fd036fb29494abc9827
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-11-03 15:22:38 +0100

    Update build_and_copy.sh

--- client/scripts/build_and_copy.sh
+++ client/scripts/build_and_copy.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-(cd client && ../gradlew build && cp -v ./build/libs/extender-client-0.0.1.jar $DYNAMO_HOME/../../com.dynamo.cr/com.dynamo.cr.common/ext/extender-client-0.0.1.jar)
+(cd client && ../gradlew build && cp -v ./build/libs/extender-client-0.0.5.jar $DYNAMO_HOME/../../com.dynamo.cr/com.dynamo.cr.common/ext/extender-client-0.0.5.jar)
