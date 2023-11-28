a5edc94da877bb26a060b90955e2adb41234b94f
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-11-28 15:29:38 +0100

    Added dev setup scripts

---
+++ server/scripts/publish-darwin-dev.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR=${SCRIPT_DIR}/../..
+
+VERSION=$(date "+%Y%m%d_%H%M")
+
+PRODUCTION_VARIANT=dev
+TARGET_HOST_URL=build-darwin-${PRODUCTION_VARIANT}.defold.com
+TARGET_HOST=i-0d371683f5c876b73
+TARGET_USER=ec2-user
+TARGET_DIR=/usr/local/extender-${PRODUCTION_VARIANT}
+TARGET_KEY=~/.ssh/defold2_ec2.pem
+
+source ${SCRIPT_DIR}/standalone/publish-standalone.sh
+
+check_uncommitted_changes ${SOURCE_DIR}
+build_artifact ${SOURCE_DIR}
+deploy_artifact ${SOURCE_DIR} ${TARGET_DIR} ${VERSION} ${TARGET_HOST} ${TARGET_USER} ${TARGET_KEY} ${PRODUCTION_VARIANT}
+
+SERVER=https://${TARGET_HOST_URL}
+
+echo "**********************************"
+echo "Checking the server version at ${SERVER}:"
+wget -q -O - $SERVER
+echo ""
+echo "**********************************"
