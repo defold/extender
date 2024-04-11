3e76a866288943741761b30aaee8379020329d1b
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2024-04-11 11:48:39 +0300

    Update test data accodring to changes from extension-html5.

--- server/test-data/ext/lib/js-web/library_dummy.js
+++ server/test-data/ext/lib/js-web/library_dummy.js
@@ -3,44 +3,44 @@
 
 var LibraryMyTest = {
 
-        // This can be accessed from the bootstrap code in the .html file
-        $MYTESTLIBRARY: {
-            _data: '',
-            _cstr: null,
+    // This can be accessed from the bootstrap code in the .html file
+    $MYTESTLIBRARY: {
+        _data: '',
+        _cstr: null,
 
-            GetTestData : function() {
-                if (typeof window !== 'undefined') {
-                    return MYTESTLIBRARY._data;
-                }
-                else {
-                    return '';
-                }
-            },
-
-            SetTestData : function(data) {
-                if (typeof window !== 'undefined') {
-                    MYTESTLIBRARY._data = data;
-                }
-            },
-        },
-
-        // These can be called from within the extension, in C++
-        testGetUserData: function() {
-            if (null == MYTESTLIBRARY._cstr) {
-                var str = MYTESTLIBRARY.GetTestData();                 // get the data from java script
-                if (str != '') {
-                    MYTESTLIBRARY._cstr = _malloc(str.length + 1);         // allocate C++ memory to store it in
-                    Module.writeStringToMemory(str, MYTESTLIBRARY._cstr);  // copy the data into the C array
-                }
+        GetTestData : function() {
+            if (typeof window !== 'undefined') {
+                return MYTESTLIBRARY._data;
             }
-            return MYTESTLIBRARY._cstr;
+            else {
+                return '';
+            }
         },
 
-        testClearUserData: function() {
-            MYTESTLIBRARY._data = ''
-            MYTESTLIBRARY._cstr = null
+        SetTestData : function(data) {
+            if (typeof window !== 'undefined') {
+                MYTESTLIBRARY._data = data;
+            }
+        },
+    },
+
+    // These can be called from within the extension, in C++
+    testGetUserData: function() {
+        if (null == MYTESTLIBRARY._cstr) {
+            var str = MYTESTLIBRARY.GetTestData();                 // get the data from java script
+            if (str != '') {
+                MYTESTLIBRARY._cstr = stringToNewUTF8(str);         // allocate C++ memory to store it in
+            }
         }
+        return MYTESTLIBRARY._cstr;
+    },
+
+    testClearUserData: function() {
+        MYTESTLIBRARY._data = '';
+        _free(MYTESTLIBRARY._cstr);
+        MYTESTLIBRARY._cstr = null;
+    }
 }
 
 autoAddDeps(LibraryMyTest, '$MYTESTLIBRARY');
-mergeInto(LibraryManager.library, LibraryMyTest);
+addToLibrary(LibraryMyTest);
