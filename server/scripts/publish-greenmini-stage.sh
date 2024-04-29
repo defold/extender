43b9e666832b5b49448539c05d2a8c2c0727515a
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-04-29 18:51:26 +0200

    Removed code duplication when calling commands

---
+++ server/scripts/publish-greenmini-stage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SOURCE_DIR=${SCRIPT_DIR}/../..
+
+VERSION=$(date "+%Y%m%d_%H%M")
+
+PRODUCTION_VARIANT=stage
+TARGET_HOST_URL=45.148.170.135
+TARGET_HOST=${TARGET_HOST_URL}
+TARGET_USER=bjorn
+TARGET_DIR=/usr/local/extender-${PRODUCTION_VARIANT}
+# TARGET_KEY=~/.ssh/defold2_ec2.pem
+
+source ${SCRIPT_DIR}/standalone/publish-standalone.sh
+
+# check_uncommitted_changes ${SOURCE_DIR}
+build_artifact ${SOURCE_DIR}
+deploy_artifact ${SOURCE_DIR} ${TARGET_DIR} ${VERSION} ${TARGET_HOST} ${TARGET_USER} ${PRODUCTION_VARIANT}
+
+SERVER=https://${TARGET_HOST_URL}
+
+echo "**********************************"
+echo "Checking the server version at ${SERVER}:"
+wget -q -O - $SERVER
+echo ""
+echo "**********************************"
