a18de323a28bd8423b4c7f6ed737403bfe9fe44a
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2024-10-15 15:53:28 +0300

    Add custom log enhancer to add more information to GCP LogEntry.

---
+++ server/src/main/java/com/defold/extender/log/ExtenderLogEnhancerConfiguration.java
@@ -0,0 +1,44 @@
+package com.defold.extender.log;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import com.google.cloud.MonitoredResource;
+import com.google.cloud.spring.core.DefaultGcpProjectIdProvider;
+
+@Component
+public class ExtenderLogEnhancerConfiguration {
+    
+    private String instanceName;
+    private String applicationName;
+    private String instanceLocation;
+    private String projectId;
+    private MonitoredResource monitoredResource;
+
+    private static ExtenderLogEnhancerConfiguration instance;
+    private ExtenderLogEnhancerConfiguration(@Autowired Environment env) {
+        if (instance != null) {
+            throw new IllegalStateException("ExtenderLogEnhancerConfiguration already initialized");
+        }
+        instance = this;
+        this.instanceName = env.getProperty("management.metrics.tags.instance", "unknown");
+        this.applicationName = env.getProperty("management.metrics.tags.application", "unknown");
+        this.instanceLocation = env.getProperty("extender.logging.instance-location", "europe-west1-b");
+        this.projectId = new DefaultGcpProjectIdProvider().getProjectId();
+        this.monitoredResource = MonitoredResource.newBuilder("generic_node")
+                                            .addLabel("project_id", projectId)
+                                            .addLabel("location", instanceLocation)
+                                            .addLabel("namespace", applicationName)
+                                            .addLabel("node_id", instanceName)
+                                            .build();
+    }
+
+    public static boolean isInitialized() {
+        return instance != null;
+    }
+
+    public static MonitoredResource getMonitoredResource() {
+        return instance != null ? instance.monitoredResource : null;
+    }
+}
