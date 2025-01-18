2a1925afe1ee1660de276dfd4f4f41c922f25478
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2025-01-18 15:17:03 +0200

    Fix application startup. Add feature toggle for sdk checksum verification.

--- server/src/main/java/com/defold/extender/services/DefoldSdkServiceConfiguration.java
+++ server/src/main/java/com/defold/extender/services/DefoldSdkServiceConfiguration.java
@@ -5,32 +5,26 @@ import java.nio.file.Path;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.experimental.SuperBuilder;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@SuperBuilder(toBuilder = true)
 @Component
 @ConfigurationProperties(prefix = "extender.sdk")
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
 public class DefoldSdkServiceConfiguration {
     private Path location;
-    private int cacheSize;
-    @Builder.Default private int mappingsCacheSize = 20;
-    private boolean cacheClearOnExit;
     private String[] sdkUrls;
     private String[] mappingsUrls;
+    private int cacheSize;
+    @Builder.Default private int mappingsCacheSize = 20;
     // retry count in case of checksum validation fail
     @Builder.Default private int maxVerificationRetryCount = 3;
-
-    // protected DefoldSdkServiceConfiguration(DefoldSdkServiceConfiguration copy) {
-    //     this.location = copy.location;
-    //     this.cacheSize = copy.cacheSize;
-    //     this.mappingsCacheSize = copy.mappingsCacheSize;
-    //     this.cacheClearOnExit = copy.cacheClearOnExit;
-    //     this.sdkUrls = copy.sdkUrls;
-    //     this.mappingsUrls = copy.;
-    //     this.maxVerificationRetryCount = 3;
-    // }
+    private boolean cacheClearOnExit;
+    private boolean enableSdkVerification;
 }
