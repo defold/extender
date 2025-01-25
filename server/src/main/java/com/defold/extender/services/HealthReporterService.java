82e155afdc5c5a96c1924e06414a0a6a8f01cfdb
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2025-01-25 14:55:43 +0200

    Collect health report concurrently. Possible fix for connection timeout.

--- server/src/main/java/com/defold/extender/services/HealthReporterService.java
+++ server/src/main/java/com/defold/extender/services/HealthReporterService.java
@@ -4,15 +4,19 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -35,18 +39,13 @@ public class HealthReporterService {
 
     private GCPInstanceService instanceService = null;
     private final HttpClient httpClient;
+    private static final Logger LOGGER = LoggerFactory.getLogger(HealthReporterService.class);
 
     public HealthReporterService(Optional<GCPInstanceService> instanceService,
                             @Autowired Tracer tracer,
                             @Autowired Propagator propogator) {
         instanceService.ifPresent(val -> { this.instanceService = val; });
-        RequestConfig config = RequestConfig.custom()
-            .setConnectTimeout(connectionTimeout)
-            .setConnectionRequestTimeout(connectionTimeout)
-            .setSocketTimeout(connectionTimeout)
-            .build();
         this.httpClient = HttpClientBuilder.create()
-            .setDefaultRequestConfig(config)
             .addInterceptorLast(new ExtenderTracerInterceptor(tracer, propogator))
             .build();
     }
@@ -59,40 +58,52 @@ public class HealthReporterService {
             Map<String, OperationalStatus> platformOperationalStatus = new HashMap<>();
             JSONObject result = new JSONObject();
             JSONParser parser = new JSONParser();
+            Map<String, CompletableFuture<Boolean>> reportResults = new HashMap<>(remoteBuilderPlatformMappings.size());
             for (Map.Entry<String, RemoteInstanceConfig> entry : remoteBuilderPlatformMappings.entrySet()) {
-                String platform = getPlatform(entry.getKey());
-                String instanceId = entry.getValue().getInstanceId();
-                // if instance controlled by instanceService and is currently suspended or suspending - mark it as 'operational'
-                if (instanceService != null && instanceService.isInstanceControlled(instanceId)) {
-                    if (instanceService.isInstanceSuspended(instanceId) 
-                    || instanceService.isInstanceSuspending(instanceId)
-                    || instanceService.isInstanceStarting(instanceId)) {
-                        updateOperationalStatus(platformOperationalStatus, platform, true);
-                        continue;
-                    } else if (!instanceService.isInstanceRunning(instanceId)) {
-                        updateOperationalStatus(platformOperationalStatus, platform, false);
-                        continue;
-                    }
-                }
-                final String healthUrl = String.format("%s/health_report", entry.getValue().getUrl());
-                final HttpGet request = new HttpGet(healthUrl);
-                try {
-                    HttpResponse response = httpClient.execute(request);
-                    if (response.getStatusLine().getStatusCode() == org.apache.http.HttpStatus.SC_OK) {
-                        JSONObject responseBody = (JSONObject)parser.parse(EntityUtils.toString(response.getEntity()));
-                        if (responseBody.containsKey("status") 
-                            && OperationalStatus.valueOf((String)responseBody.get("status")) == OperationalStatus.Operational) {
-                            updateOperationalStatus(platformOperationalStatus, platform, true);
-                        } else {
-                            updateOperationalStatus(platformOperationalStatus, platform, false);
+                CompletableFuture<Boolean> statusRequest = CompletableFuture.supplyAsync(() -> {
+                    String instanceId = entry.getValue().getInstanceId();
+                    // if instance controlled by instanceService and is currently suspended or suspending - mark it as 'operational'
+                    if (instanceService != null && instanceService.isInstanceControlled(instanceId)) {
+                        if (instanceService.isInstanceSuspended(instanceId)
+                        || instanceService.isInstanceSuspending(instanceId)
+                        || instanceService.isInstanceStarting(instanceId)) {
+                            return Boolean.TRUE;
+                        } else if (!instanceService.isInstanceRunning(instanceId)) {
+                            return Boolean.FALSE;
                         }
-                    } else {
-                        EntityUtils.consumeQuietly(response.getEntity());
-                        updateOperationalStatus(platformOperationalStatus, platform, false);
                     }
-                } catch(Exception exc) {
-                    updateOperationalStatus(platformOperationalStatus, platform, false);
+                    final String healthUrl = String.format("%s/health_report", entry.getValue().getUrl());
+                    final HttpGet request = new HttpGet(healthUrl);
+                    try {
+                        HttpResponse response = httpClient.execute(request);
+                        if (response.getStatusLine().getStatusCode() == org.apache.http.HttpStatus.SC_OK) {
+                            JSONObject responseBody = (JSONObject)parser.parse(EntityUtils.toString(response.getEntity()));
+                            if (responseBody.containsKey("status")
+                                && OperationalStatus.valueOf((String)responseBody.get("status")) == OperationalStatus.Operational) {
+                                    return Boolean.TRUE;
+                            } else {
+                                return Boolean.FALSE;
+                            }
+                        } else {
+                            EntityUtils.consumeQuietly(response.getEntity());
+                            return Boolean.FALSE;
+                        }
+                    } catch(Exception exc) {
+                        return Boolean.FALSE;
+                    }
+                });
+                statusRequest.completeOnTimeout(Boolean.FALSE, this.connectionTimeout, TimeUnit.MILLISECONDS);
+                reportResults.put(entry.getKey(), statusRequest);
+            }
+            for (Map.Entry<String, CompletableFuture<Boolean>> status : reportResults.entrySet()) {
+                String platform = getPlatform(status.getKey());
+                Boolean isInstanceReachable = false;
+                try {
+                    isInstanceReachable = status.getValue().get();
+                } catch(InterruptedException|ExecutionException exc) {
+                    LOGGER.error("Error during request health status.", exc);
                 }
+                updateOperationalStatus(platformOperationalStatus, platform, isInstanceReachable);
             }
 
             for (Map.Entry<String, OperationalStatus> entry : platformOperationalStatus.entrySet()) {
