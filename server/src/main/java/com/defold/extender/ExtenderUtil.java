1d59cc11617565016324f3a6d16a1fd67aa43505
Kharkunov Eugene <eugene.kharkunov@gmail.com>
2025-04-25 09:08:42 +0300

    Updated naming

--- server/src/main/java/com/defold/extender/ExtenderUtil.java
+++ server/src/main/java/com/defold/extender/ExtenderUtil.java
@@ -801,7 +801,7 @@ public class ExtenderUtil
 
     public static boolean isWebTarget(String platform) {
         return platform.equals("wasm-web") ||
-               platform.equals("wasm_pthreads-web") ||
+               platform.equals("wasm_pthread-web") ||
                platform.equals("js-web");
     }
 
